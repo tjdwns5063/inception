75b0134bb550466b7ac442e8dd9c440d5a09b01d
seongjki <tjdwns5063@naver.com>
2022-10-06 04:07:28 -0400

    revise

--- srcs/requirements/wordpress/tools/setup.sh
+++ srcs/requirements/wordpress/tools/setup.sh
@@ -3,3 +3,5 @@
 # setup
 wget http://wordpress.org/latest.zip -P /var/www/html/wordpress
 unzip /var/www/html/wordpress/latest.zip
+mv /var/www/html/wordpress/wordpress/* /var/www/html/wordpress
+rmdir /var/www/html/wordpress/wordpress
