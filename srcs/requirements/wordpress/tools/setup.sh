ee9187206a1067fcb7632c5eb4d64e7dad133b16
seongjki <tjdwns5063@naver.com>
2022-10-06 01:04:35 -0400

    commit

--- srcs/requirements/wordpress/tools/setup.sh
+++ srcs/requirements/wordpress/tools/setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # setup
-wget http://wordpress.org/latest.zip -P /var/www/html/
-unzip /var/www/html/latest.zip -d /var/www/html/
+wget http://wordpress.org/latest.zip -P /var/www/html/wordpress
+unzip /var/www/html/wordpress/latest.zip
